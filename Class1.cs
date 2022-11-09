823a3dba0d70e8d1edcac0b66210d9321eff6d5c
Dmitry010101 <112878418+Dmitry010101@users.noreply.github.com>
2022-11-09 19:49:48 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace cakes
+{
+    internal class menuu
+    {
+        public string desc;
+        public int cost;
+
+        public menuu(string desc, int cost)
+        {
+            this.desc = desc;
+            this.cost = cost;
+        }
+        public void per()
+        {
+            Console.WriteLine(desc + " - " + cost);
+        }
+    }
+}
+

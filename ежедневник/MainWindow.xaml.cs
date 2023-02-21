931b587a811808d8c684b85998fc8195c504c4dc
Dmitry010101 <112878418+Dmitry010101@users.noreply.github.com>
2023-02-21 22:01:08 +0300

    Add files via upload

---
+++ ежедневник/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+namespace Ejednevnik
+{
+    public partial class MainWindow : Window
+    {
+        string data;
+        readonly List<Zametka> zametki1 = new();
+        readonly List<Zametka> zametki2 = new();
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public MainWindow()
+        {
+            InitializeComponent();
+            DP.Text = DateTime.Now.ToString();
+        }
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        private void Clear() 
+        {
+            NZ.Text = "";
+            Opisanie.Text = "";
+        }
+        private void Conclusion()
+        {
+            zametki1.Clear();
+            foreach (Zametka i in zametki2)
+            {
+                if (i.data == data)
+                {
+                    zametki1.Add(i);
+                    Z.Items.Add(i.name);
+                }
+            }
+        }
+        private void Add(object sender, RoutedEventArgs e)
+        {
+             Zametka new_zam = new(NZ.Text, Opisanie.Text, DP.Text);
+             zametki2.Add(new_zam);
+             Z.Items.Add(new_zam.name);
+             zametki1.Add(new_zam);
+             Clear();
+        }
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+        private void SelectedDate(object sender, SelectionChangedEventArgs e)
+        {
+            data = DP.Text;
+            Clear();
+            foreach (Zametka i in zametki1)
+            {
+                Z.Items.Remove(i.name);
+            }
+            Conclusion();
+        }
+        private void Selection(object sender, SelectionChangedEventArgs e)
+        {
+            int RZ = 0;
+            if (Z.SelectedIndex != -1)
+            {
+                NZ.Text = zametki1[RZ].name;
+                Opisanie.Text = zametki1[RZ].opis;
+            }
+        }
+    }
+}

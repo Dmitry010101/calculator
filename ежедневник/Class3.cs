06c91c6f2cce497859b7d018471cd1481e53049e
Dmitry010101 <112878418+Dmitry010101@users.noreply.github.com>
2023-02-21 21:20:13 +0300

    Add files via upload

---
+++ ежедневник/Class3.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejednevnik
+{
+    public class Zametka
+    {
+        public string name;
+        public string opis;
+        public string data;
+        public Zametka(string name_, string opis_, string data_)
+        {
+            name = name_;
+            opis = opis_;
+            data = data_;
+        }
+    }
+}
